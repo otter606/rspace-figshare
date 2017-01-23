578c7969b38f71ced0a23ab7fd2b8d1522bd58df
Richard Adams <ra22597@gmail.com>
2017-01-23 21:34:51 +0000

    Commit all

--- src/test/java/com/researchspace/rspace/figshare/RSpaceFigshareConnector.java
+++ src/test/java/com/researchspace/rspace/figshare/RSpaceFigshareConnector.java
@@ -20,62 +20,64 @@ import com.researchspace.apimodel.Field;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
-@TestPropertySource(locations="classpath:test.properties")
-@ContextConfiguration(classes=RSpaceFigshareConnector.class)
+@TestPropertySource(locations = "classpath:test.properties")
+@ContextConfiguration(classes = RSpaceFigshareConnector.class)
 @Slf4j
-//@Configuration
-public class RSpaceFigshareConnector extends AbstractJUnit4SpringContextTests  {
-	
-	@Autowired Environment env;
-	
-	
+// @Configuration
+public class RSpaceFigshareConnector extends AbstractJUnit4SpringContextTests {
+
+	@Autowired
+	Environment env;
+
 	String figshareToken = "";
-    String rspaceToken = "";
-    String rspaceUrl = "";
-    RestTemplate template ;
+	String rspaceToken = "";
+	String rspaceUrl = "";
+	RestTemplate template;
+
 	@Test
 	public void test() throws InterruptedException {
 		template = new RestTemplate();
 		// these properties can be defined on command line,
-		// in gradle.properties as systemProp.properties or in test.properties file.
-		this.rspaceUrl= env.getProperty("rspace.url");
+		// in gradle.properties as systemProp.properties or in test.properties
+		// file.
+		this.rspaceUrl = env.getProperty("rspace.url");
 		this.rspaceToken = env.getProperty("rspaceToken");
 		this.figshareToken = env.getProperty("figshareToken");
-//		FigshareTemplate figshare = new FigshareTemplate(figshareToken);
-//		figshare.setPersonalToken(figshareToken);
-//		Account acc = figshare.account();
-//		System.out.println(acc.getEmail());
+		// FigshareTemplate figshare = new FigshareTemplate(figshareToken);
+		// figshare.setPersonalToken(figshareToken);
+		// Account acc = figshare.account();
+		// System.out.println(acc.getEmail());
 		getDocuments();
 	}
-	
+
 	void getDocuments() throws InterruptedException {
 		HttpHeaders headers = createHeadersWithAPiKey();
 		String url = rspaceUrl + "/documents?pageSize=5";
 		HttpEntity<String> ent = new HttpEntity<>(headers);
-		ResponseEntity<DocumentList > resp =template.exchange(url,HttpMethod.GET,ent,DocumentList.class);
+		ResponseEntity<DocumentList> resp = template.exchange(url, HttpMethod.GET, ent, DocumentList.class);
 
-	    log.debug(resp.getBody().toString());
-	    
-	    for (Document doc: resp.getBody().getDocuments()) {
-	    	Long id = doc.getId();
-	    	Thread.sleep(1000);
-	    	
-	    	Document fullDoc = null;
-	    	try{
-	    		fullDoc = getDocument(id);
-	    	} catch(Exception e) {
-	    		log.warn("Error retrieving doc " + id);
-	    		log.warn(e.getMessage());
-	    		continue;
-	    	}
-	    	
-	    	for (Field f: fullDoc.getFields()) {
-	    		if (f.getFiles().size() > 0){
-	    			log.info(String.format("Field %s in document %s has %d files", f.getName(), doc.getName(),
-	    		f.getFiles().size()));
-	    		}
-	    	}
-	    }
+		log.debug(resp.getBody().toString());
+
+		for (Document doc : resp.getBody().getDocuments()) {
+			Long id = doc.getId();
+			Thread.sleep(1000);
+
+			Document fullDoc = null;
+			try {
+				fullDoc = getDocument(id);
+			} catch (Exception e) {
+				log.warn("Error retrieving doc " + id);
+				log.warn(e.getMessage());
+				continue;
+			}
+
+			for (Field f : fullDoc.getFields()) {
+				if (f.getFiles().size() > 0) {
+					log.info(String.format("Field %s in document %s has %d files", f.getName(), doc.getName(),
+							f.getFiles().size()));
+				}
+			}
+		}
 	}
 
 	private HttpHeaders createHeadersWithAPiKey() {
@@ -86,9 +88,9 @@ public class RSpaceFigshareConnector extends AbstractJUnit4SpringContextTests  {
 
 	private Document getDocument(Long id) {
 		log.info("Retrieving doc " + id);
-		String url = rspaceUrl + "/documents/" +id;
+		String url = rspaceUrl + "/documents/" + id;
 		HttpEntity<String> ent = new HttpEntity<>(createHeadersWithAPiKey());
-		ResponseEntity<Document > resp =template.exchange(url,HttpMethod.GET,ent,Document.class);
+		ResponseEntity<Document> resp = template.exchange(url, HttpMethod.GET, ent, Document.class);
 		return resp.getBody();
 	}
 
